812fb7f826c2eb42d32353fc8fa73017d33e3964
sujal gupta <sujalgupta1221@gmail.com>
2023-02-24 18:19:21 +0530

    Ready for deployment

--- src/features/auth/PersistLogin.js
+++ src/features/auth/PersistLogin.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useEffect, useRef, useState } from 'react'
 import { Outlet, Link } from "react-router-dom"
 import { useRefreshMutation } from "./authApiSlice"
--- src/features/notes/NewNoteForm.js
+++ src/features/notes/NewNoteForm.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useState, useEffect } from "react"
 import { useAddNewNoteMutation } from './notesApiSlice'
 import { useNavigate } from "react-router-dom"
