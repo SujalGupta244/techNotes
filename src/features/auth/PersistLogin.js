96fd0b731b25eddf59a5ff8ab9d135fa9cdf04d2
sujal gupta <sujalgupta1221@gmail.com>
2023-02-25 11:27:16 +0530

    Ready for deployment

--- src/features/auth/PersistLogin.js
+++ src/features/auth/PersistLogin.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { useEffect, useRef, useState } from 'react'
 import { Outlet, Link } from "react-router-dom"
+import { useEffect, useRef, useState } from 'react'
 import { useRefreshMutation } from "./authApiSlice"
 import usePersist from "../../hooks/usePersist"
 import { useSelector } from 'react-redux'
 import { selectCurrentToken } from "./authSlice"
+import PulseLoader from 'react-spinners/PulseLoader'
 
 const PersistLogin = () => {
 
     const [persist] = usePersist()
     const token = useSelector(selectCurrentToken)
-    const effectRan = useRef(false) // it should be false
+    const effectRan = useRef(false)
 
     const [trueSuccess, setTrueSuccess] = useState(false)
 
@@ -43,44 +43,34 @@ const PersistLogin = () => {
             if (!token && persist) verifyRefreshToken()
         }
 
-        // return () => effectRan.current = true
-        return () => { // ITs cleanup is NOT Working idk why
-            effectRan.current = true
-            console.log("useEffect return");
-        }
+        return () => effectRan.current = true
 
         // eslint-disable-next-line
     }, [])
 
 
-
     let content
-    if (!persist) { 
-        // persist: no
+    if (!persist) { // persist: no
         console.log('no persist')
-        content = (<Outlet />)
-    } else if (isLoading) { 
-        //persist: yes, token: no
+        content = <Outlet />
+    } else if (isLoading) { //persist: yes, token: no
         console.log('loading')
-        content = (<p>Loading...</p>)
-    } else if (isError) { 
-        //persist: yes, token: no
+        content = <PulseLoader color={"#FFF"} />
+    } else if (isError) { //persist: yes, token: no
         console.log('error')
         content = (
             <p className='errmsg'>
                 {`${error?.data?.message} - `}
-                <Link to="/login"> Please login again</Link>.
+                <Link to="/login">Please login again</Link>.
             </p>
         )
-    } else if (isSuccess && trueSuccess) { 
-        //persist: yes, token: yes
+    } else if (isSuccess && trueSuccess) { //persist: yes, token: yes
         console.log('success')
-        content = (<Outlet />)
-    } else if (token && isUninitialized) { 
-        //persist: yes, token: yes
+        content = <Outlet />
+    } else if (token && isUninitialized) { //persist: yes, token: yes
         console.log('token and uninit')
         console.log(isUninitialized)
-        content = (<Outlet />)
+        content = <Outlet />
     }
 
     return content
